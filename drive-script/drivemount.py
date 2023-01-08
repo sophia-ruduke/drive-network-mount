b4cbfd46ebf2a33b57de423d844778dbb94ae805
sophia-ruduke <sophia.ruduke@kymerasystems.com>
2023-01-07 20:36:27 -0700

    No longer terminates upon non-existent interface in config.yml & overall cleanup of code

--- drive-script/drivemount.py
+++ drive-script/drivemount.py
@@ -3,14 +3,16 @@ import random,os,string
 import ctypes
 import ctypes.util
 import yaml
-#import psutil
 import sh
+#import psutil
 
 
 libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
 libc.mount.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_ulong, ctypes.c_char_p)
-
 b64chars = string.ascii_letters+string.digits+"-_"
+
+mounts = {}
+
 
 def getId(length=11,chars=b64chars):
     return "".join([random.choice(chars) for i in range(length)])
@@ -46,20 +48,24 @@ def network_config(node, new_changes):
     if config is not None and 'network' in config:
         for interface in config['network']:
             params = config['network'][interface][0]
-
-            if new_changes:
-                sh.ip("addr", "add", params['address'], "dev", interface)
-                # parse through output to get current gateway config
-                current_route = sh.ip("r", "show", "dev", interface)
-                current_route = str(current_route).split("\n")[0]
-                mounts[node]["gateway"] = current_route.split()[2]
-                sh.ip("route", "delete", "default")
-                sh.ip("route", "add", "default", "via", params['gateway'])
-                sh.resolvectl("dns", interface, params['dns'])
-            else:
-                sh.ip("addr", "delete", params['address'], "dev", interface)
-                sh.ip("route", "delete", "default")
-                sh.ip("route", "add", "default", "via", mounts[node]["gateway"])
+            interface_dir = f"/sys/class/net/{interface}"
+            # change IP, DNS, and gateway settings only if the interface
+            # in the config.yml exists on this device
+            if os.path.exists(interface_dir):
+                if new_changes:
+                    sh.ip("addr", "add", params['address'], "dev", interface)
+                    # parse through output to get current gateway config
+                    current_route = sh.ip("r", "show", "dev", interface)
+                    current_route = str(current_route).split("\n")[0]
+                    mounts[node]["gateway"] = current_route.split()[2]
+                    
+                    sh.ip("route", "delete", "default")
+                    sh.ip("route", "add", "default", "via", params['gateway'])
+                    sh.resolvectl("dns", interface, params['dns'])
+                else:
+                    sh.ip("addr", "delete", params['address'], "dev", interface)
+                    sh.ip("route", "delete", "default")
+                    sh.ip("route", "add", "default", "via", mounts[node]["gateway"])
                 
 def url_setup(node):
     config = parse_config(node)
@@ -70,28 +76,28 @@ def url_setup(node):
         new_url = config['ignition']['project-url']
         sh.snap("set", "wpe-webkit-mir-kiosk", f"url={new_url}")
 
-context = pyudev.Context()
-monitor = pyudev.Monitor.from_netlink(context)
-monitor.filter_by(subsystem='block')
+def main():
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem='block')
 
-original_gateway = None
-mounts = {}
+    for device in iter(monitor.poll, None):
+        if device.action == 'add':
+            if device.device_type == "partition":
+                mnt_dir = f'/mnt/usb/{getId()}'
+                node = device.device_node
+                os.makedirs(mnt_dir)
 
-for device in iter(monitor.poll, None):
-    if device.action == 'add':
-        if device.device_type == "partition":
-            mnt_dir = f'/mnt/usb/{getId()}'
-            node = device.device_node
-            os.makedirs(mnt_dir)
+                if node in mounts.keys():
+                    unmount(node)
 
-            if node in mounts.keys():
-                unmount(node)
+                _ = sh.mount(node,mnt_dir)
+                mounts[node] = {"dir":mnt_dir, "config":parse_config(mnt_dir)}
+                network_config(node, True)
 
-            _ = sh.mount(node,mnt_dir)
-            mounts[node] = {"dir":mnt_dir, "config":parse_config(mnt_dir)}
-            network_config(node, True)
+        elif device.action == 'remove':
+            if device.device_type == "partition":
+                unmount(device.device_node)
 
-    elif device.action == 'remove':
-        if device.device_type == "partition":
-            unmount(device.device_node)
-            
+if __name__ == "__main__":
+    main()
